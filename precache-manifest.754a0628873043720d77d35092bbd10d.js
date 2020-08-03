ca284e339f33dcbdadbbaf06c4adaf9bbdc8cc40
Phi Henry Nguyen <phi.ng1114@gmail.com>
2020-08-02 22:11:47 -0500

    Updates

---
+++ precache-manifest.754a0628873043720d77d35092bbd10d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c903d09b37becc1fc676fcf3c7e9113",
+    "url": "/bingo/index.html"
+  },
+  {
+    "revision": "5b63d5aa4de400166c42",
+    "url": "/bingo/static/css/main.cd60315b.chunk.css"
+  },
+  {
+    "revision": "1e5840a5e2f6f14d245f",
+    "url": "/bingo/static/js/2.169a9c60.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/bingo/static/js/2.169a9c60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b63d5aa4de400166c42",
+    "url": "/bingo/static/js/main.cc9898f7.chunk.js"
+  },
+  {
+    "revision": "c9787d242660ef1b686b",
+    "url": "/bingo/static/js/runtime-main.567b1349.js"
+  }
+]);
